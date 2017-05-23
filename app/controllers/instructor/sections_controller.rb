e8c37bb074ed41ed4abcd17c022fd2d1d1773494
rxso <rajat.soni@icloud.com>
2017-05-23 18:58:31 +0000

    Add period to line 46 in app/views/courses/show.html.erb

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -32,7 +32,7 @@ class Instructor::SectionsController < ApplicationController
   
   def require_authorized_for_current_course
     if current_course.user != current_user 
-      render plain: 'Unauthorized', status: :unauthorized
+      render plain: 'Changes Not Permitted', status: :unauthorized
     end
   end
   
